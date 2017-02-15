6586e46d52c76989c5a397bd6b6533cf51b21ac4
SheaClose <SheaClose@gmail.com>
2017-02-15 10:29:05 -0600

    map now showing on map page based on zip input

--- app/controllers/MapCtrl.js
+++ app/controllers/MapCtrl.js
@@ -1,5 +1,5 @@
 angular.module("app")
-.controller("mapCtrl", function($scope, $state){
+.controller("mapCtrl", function($scope, $state, mapFactory){
 	$scope.map = {
 			center: {
 					latitude: $state.params.lat
@@ -7,5 +7,17 @@ angular.module("app")
 			}
 		, zoom: 12
 	};
-
+	if(!$state.params.lat && !$state.params.lng){
+		(function(){
+			mapFactory.searchByLocation().then(response=>{
+				$scope.map = {
+						center: {
+								latitude: response.lat
+							, longitude: response.lng
+						}
+					, zoom: 12
+				}
+			})
+		})()
+	}
 })
--- app/factories/mapFactory.js
+++ app/factories/mapFactory.js
@@ -2,9 +2,14 @@ angular.module("app")
 .factory("mapFactory", function($http){
 	return {
 		searchByZipcode: function(zipCodeInput){
-	    return $http.get("https://maps.googleapis.com/maps/api/geocode/json?address=" + zipCodeInput + "&key=AIzaSyAXQB6_1o2TlMK7coZPgfuVDAdxZtPiv9c").then(function(response){
+	    return $http.get("https://maps.googleapis.com/maps/api/geocode/json?address=" + zipCodeInput + "&key=AIzaSyD48MJ_f0TCqUdP7VzwwEB_PZiz-dInePs").then(function(response){
 	      return response.data.results[0].geometry.location
 	    })
-	  }
+	  },
+		searchByLocation: function(){
+			return $http.post("https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyD48MJ_f0TCqUdP7VzwwEB_PZiz-dInePs").then(function(response){
+	      return response.data.location
+	    })
+		}
 	}
 })
