5aa030eea90e3b41a427e997ef3c7ae3634118df
SheaClose <SheaClose@gmail.com>
2017-03-05 12:56:12 -0600

    pushing

--- server/breweries/breweryRoutes.js
+++ server/breweries/breweryRoutes.js
@@ -1,7 +1,6 @@
 const breweryCtrl = require("./breweryCtrl.js")
 
 module.exports = app => {
-	app.get("/breweries/:lng/:lat", breweryCtrl.getBreweriesByLocation)
 	app.get("/breweries", breweryCtrl.getBreweriesByName)
-
+	app.get("/breweries/:lng/:lat", breweryCtrl.getBreweriesByLocation)
 }
