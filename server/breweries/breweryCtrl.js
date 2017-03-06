84ae9ad49817696b36fd18802e83af76be92ecef
SheaClose <SheaClose@gmail.com>
2017-03-06 13:13:10 -0600

    testing next call in breweryCtrl

--- server/breweries/breweryCtrl.js
+++ server/breweries/breweryCtrl.js
@@ -3,13 +3,18 @@ const brewdb = require("../../brewDb.js")
 		, serverConfig = require("../../serverConfig.js")
 
 module.exports = {
-	getBreweriesByName(req, res) {
-		brewdb.search.breweries({ q: req.query.breweryName }, (err, brewery) => {
-			if (err) {
-				return res.json(err)
-			}
-			return res.status(200).json(brewery)
-		});
+	getBreweriesByName(req, res, next) {
+		if (req.query.breweryName) {
+			brewdb.search.breweries({ q: req.query.breweryName }, (err, brewery) => {
+				if (err) {
+					return res.json(err)
+				}
+				return res.status(200).json(brewery)
+			});
+		}
+		else {
+			next();
+		}
 	},
 	getBreweriesByLocation(req,res){
 		request(`https://api.brewerydb.com/v2/search/geo/point?lat=${req.params.lat}&lng=${req.params.lng}&radius=50&key=${serverConfig.brewDbKey}`, function(err, breweries, body){
