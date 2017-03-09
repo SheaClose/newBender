ab2f96a96149dbb072a0a0d0e993b56d4af1db47
SheaClose <SheaClose@gmail.com>
2017-03-09 17:06:20 -0600

    changed params to query in brewery search by location

--- server/breweries/breweryCtrl.js
+++ server/breweries/breweryCtrl.js
@@ -12,20 +12,13 @@ module.exports = {
 				return res.status(200).json(brewery)
 			});
 		}
-		return res.status(220).json({bod:req.body, query: req.query})
 		if (req.query.lng){
-			// console.log(req.query.lng)
-			return res.status(200).json({lng:req.query.lng, lat:req.query.lat})
 			request(`https://api.brewerydb.com/v2/search/geo/point?lat=${req.query.lat}&lng=${req.query.lng}&radius=50&key=${serverConfig.brewDbKey}`, function(err, breweries, body){
 				if (err) {
 					return res.status(200).json(err)
 				}
-				return res.status(200).json(breweries)
+				return res.status(200).json(body)
 			})
 		}
 	}
-	// ,
-	// getBreweriesByLocation(req,res){
-	//
-	// }
 }
