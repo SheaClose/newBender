92bbd47e02b33a27b2232215f2f000679b2ff64b
SheaClose <sheaclose@gmail.com>
2018-10-05 10:03:59 -0500

    done

--- index.js
+++ index.js
@@ -1,16 +1,57 @@
-const express = require("express")
-      , {json} = require("body-parser")
-      , cors = require("cors")
-      , serverConfig = require("./serverConfig.js")
-      , port = serverConfig.port || 3000
-      , app = express()
-      , session = require('express-session')
-			, masterRoutes = require("./masterRoutes.js")
-
-app.use(session({ secret: serverConfig.secret, resave: true, saveUninitialized:true } ) );
+const express = require("express"),
+  { json } = require("body-parser"),
+  cors = require("cors"),
+  serverConfig = require("./serverConfig.js"),
+  port = serverConfig.port || 3000,
+  app = express(),
+  session = require("express-session"),
+  masterRoutes = require("./masterRoutes.js"),
+  axios = require("axios");
+app.use(
+  session({
+    secret: serverConfig.secret,
+    resave: true,
+    saveUninitialized: true
+  })
+);
 app.use("/", express.static(__dirname));
 app.use(json());
 app.use(cors());
-masterRoutes(app)
+app.post("/api/proxy", (req, res) => {
+  let { method, data, url } = req.body;
+  switch (method) {
+    case "GET":
+      axios.get(url).then(data => {
+        res
+          .status(200)
+          .json(data)
+          .catch(err => res.status(500).json(err));
+      });
+    case "PUT":
+      axios.put(url, data).then(data => {
+        res
+          .status(200)
+          .json(data)
+          .catch(err => res.status(500).json(err));
+      });
+    case "POST":
+      axios.post(url, data).then(data => {
+        res
+          .status(200)
+          .json(data)
+          .catch(err => res.status(500).json(err));
+      });
+    case "DELETE":
+      axios.delete(url, data).then(data => {
+        res
+          .status(200)
+          .json(data)
+          .catch(err => res.status(500).json(err));
+      });
+  }
+});
+masterRoutes(app);
 
-app.listen(port, () => {console.log(`This is Dr. Crane... I'm listening. Port:${port}`)})
+app.listen(port, () => {
+  console.log(`This is Dr. Crane... I'm listening. Port:${port}`);
+});
